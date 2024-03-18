dc17fd3b80e35e5fc74e78b4e9749c0dc7390f1c
Muhammad540 <thisisdeahmed@gmail.com>
2024-03-18 16:18:12 +0300

    Added the spawning functionality

---
+++ ME461LABS_ws/src/spawn_service/spawn_service/spawn_service.py
@@ -0,0 +1,56 @@
+import rclpy
+import os
+from rclpy.node import Node
+from gazebo_msgs.srv import SpawnEntity
+from ament_index_python.packages import get_package_share_directory
+import xacro
+import random
+
+class SpawnService(Node):
+    def __init__(self):
+        super().__init__('spawn_service')
+
+        self.spawn_client = self.create_client(SpawnEntity, 'spawn_entity')
+        while not self.spawn_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+
+    def spawn(self, name, number):
+        pkg_path = os.path.join(get_package_share_directory('sokoban'))
+        xacro_file = os.path.join(pkg_path,'urdf','box_spawn.urdf.xacro')
+        robot_description_config = xacro.process_file(xacro_file)
+
+        futures = []
+        for i in range(number):
+            req = SpawnEntity.Request()
+            req.xml = robot_description_config.toxml()
+            req.robot_namespace = name
+            req.name = name + str(i)
+            req.initial_pose.position.x = random.uniform(-8, 8)
+            req.initial_pose.position.y = random.uniform(-8, 8)
+            req.initial_pose.position.z = 0.0
+            req.initial_pose.orientation.x = 0.0
+            req.initial_pose.orientation.y = 0.0
+            req.initial_pose.orientation.z = 0.0
+            req.initial_pose.orientation.w = 1.0
+            req.reference_frame = 'world'
+            future = self.spawn_client.call_async(req)
+            futures.append((future, req.name))
+
+        for future, req_name in futures:
+            rclpy.spin_until_future_complete(self, future)
+            if future.result() is not None:
+                self.get_logger().info(f'Successfully spawned {req_name}')
+            else:
+                self.get_logger().info(f'Failed to spawn {req_name}')
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    spawn_service = SpawnService()
+    spawn_service.spawn('box', 10)
+    rclpy.spin(spawn_service)
+    spawn_service.destroy_node()
+    rclpy.shutdown()
+
+
+        
