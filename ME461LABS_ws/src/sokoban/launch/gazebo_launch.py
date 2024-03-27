0bc9484345debf09986b793f5f3108b11dda5fc3
Muhammad540 <thisisdeahmed@gmail.com>
2024-03-27 22:57:19 +0300

    Enhanced the outlook of turtlebot3

--- ME461LABS_ws/src/sokoban/launch/gazebo_launch.py
+++ ME461LABS_ws/src/sokoban/launch/gazebo_launch.py
@@ -6,6 +6,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
 
 
 
@@ -56,14 +57,50 @@ def generate_launch_description():
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose,
-            'y_pose': y_pose
-        }.items()
+    # Spawning the turtlebot in the world
+    # spawn_turtlebot_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+    #     ),
+    #     launch_arguments={
+    #         'x_pose': x_pose,
+    #         'y_pose': y_pose
+    #     }.items()
+    # )
+    # Get the urdf file
+    TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+    model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
+    urdf_path = os.path.join(
+        get_package_share_directory('sokoban'),
+        'models',
+        model_folder,
+        'model.sdf'
+    )
+
+    # Launch configuration variables specific to simulation
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    # Declare the launch arguments
+    declare_x_position_cmd = DeclareLaunchArgument(
+        'x_pose', default_value='0.0',
+        description='Specify namespace of the robot')
+
+    declare_y_position_cmd = DeclareLaunchArgument(
+        'y_pose', default_value='0.0',
+        description='Specify namespace of the robot')
+    
+    start_gazebo_ros_spawner_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', TURTLEBOT3_MODEL,
+            '-file', urdf_path,
+            '-x', x_pose,
+            '-y', y_pose,
+            '-z', '0.01'
+        ],
+        output='screen',
     )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -80,8 +117,10 @@ def generate_launch_description():
         gzserver_cmd,
         gzclient_cmd,
         robot_state_publisher_cmd,
-        spawn_turtlebot_cmd,
-        spawn_entity_2,
+        declare_x_position_cmd,
+        declare_y_position_cmd,
+        start_gazebo_ros_spawner_cmd,
+        spawn_entity_2,                            #spawn_turtlebot_cmd,
     ])
 
 
