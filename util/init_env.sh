9c9d72588854eada8fd0ed5a0d560b1e1ef99925
üsâme_aw <osamasaedawad@gmail.com>
2024-05-23 01:29:04 +0300

    more path fixes(please be the last one :'( )

--- util/init_env.sh
+++ util/init_env.sh
@@ -2,9 +2,9 @@
 
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
-cd /home/robot_degilim_labs/mnt/labs_ws
+cd /home/me461/mnt/labs_ws
 colcon build --symlink-install 
 echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
-source /usr/share/gazebo/setup.sh 
+echo "source /home/me461/mnt/labs_ws/install/setup.bash" >> ~/.bashrc
 echo "export GAZEBO_MODEL_PATH=\${GAZEBO_MODEL_PATH}:/opt/ros/humble/share/humble_gazebo/models" >> ~/.bashrc
 echo "export TURTLEBOT3_MODEL=waffle" >> ~/.bashrc
