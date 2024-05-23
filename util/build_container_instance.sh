1ae7815cf7af5586b5383262d8954e6a1629d675
üsâme_aw <osamasaedawad@gmail.com>
2024-05-23 10:25:57 +0300

    minor fixes

--- util/build_container_instance.sh
+++ util/build_container_instance.sh
@@ -7,11 +7,11 @@ docker run -d \
     --privileged \
     --net=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v ~/robot_degilim_labs:/home/robot_degilim_labs/mnt/ \
+    -v ~/me461:/home/me461/mnt/ \
     -v /dev:/dev \
     -e DISPLAY=${DISPLAY} \
     -u 1000 \
-    -w /home/robot_degilim_labs/mnt/labs_ws \
+    -w /home/me461/mnt/labs_ws \
     --name me461_labs\
     --restart unless-stopped \
     me461_labs:latest \
@@ -20,7 +20,6 @@ docker run -d \
 #extra convenient stuff
 docker exec me461_labs bash -c "source /opt/ros/humble/setup.bash" 
 docker exec me461_labs bash -c "colcon build --symlink-install"
-docker exec me461_labs bash -c "echo 'source /opt/ros/humble/setup.bash' >> /home/robot_degilim_labs/.bashrc"
-docker exec me461_labs bash -c "echo 'source /home/robot_degilim_labs/mnt/labs_ws/install/setup.bash' >> /home/robot_degilim_labs/.bashrc"
-docker exec me461_labs bash -c "echo 'export PATH=\$PATH:/home/robot_degilim_labs/mnt/util/' >> /home/robot_degilim_labs/.bashrc"
-docker exec me461_labs bash -c "echo 'export ROS_DOMAIN_ID=21' >> /home/robot_degilim_labs/.bashrc"
+docker exec me461_labs bash -c "echo 'source /opt/ros/humble/setup.bash' >> /home/me461/.bashrc"
+docker exec me461_labs bash -c "echo 'source /home/me461/mnt/labs_ws/install/setup.bash' >> /home/me461/.bashrc"
+docker exec me461_labs bash -c "echo 'export PATH=\$PATH:/home/me461/mnt/util/' >> /home/me461/.bashrc"
